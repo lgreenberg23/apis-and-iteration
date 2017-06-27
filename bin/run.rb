a9f05b25338d53c70845a41740546702a27ceee0
Luis Arita <github@luisarita.com>
2017-06-27 16:48:30 -0400

    First Commit

--- bin/run.rb
+++ bin/run.rb
@@ -3,6 +3,20 @@
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
 
+def menu
+  choice = get_user_choice
+  case choice
+  when "1"
+    character = get_character_from_user
+    show_character_movies(character)
+  when "2"
+    movie = get_movie_from_user
+    show_movie_crawl(movie)
+  else
+    puts "Please enter 1 or 2"
+    menu
+  end
+end
+
 welcome
-character = get_character_from_user
-show_character_movies(character)
+menu
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,46 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character)
+def get_character_from_api_by_url(character, url)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  all_characters = RestClient.get(url)
   character_hash = JSON.parse(all_characters)
-  
+  #luke_skywalker = character_hash["results"][0]["name"]
+  found_character = character_hash["results"].find {|character_data| character_data["name"] == character }
+
+  while found_character == nil
+    url = character_hash["next"]
+    if url != null
+      found_character = get_character_from_api_by_url(character, url)
+    end
+  end
+
+  found_character
+end
+
+=begin
+def cycle_page(character)
+entry_character = nil
+found_character = entry_character
+while found_character == nil
+  found_character = character
+  url = character_hash["next"]
+  found_character = get_character_from_api_by_url(character, url)
+end
+found_character
+end
+=end
+
+
+def get_character_movies_from_api(character)
+  url = 'http://www.swapi.co/api/people/'
+  found_character = get_character_from_api_by_url(character, url)
+
+  found_character["films"].collect do |film_url|
+    film_info = RestClient.get(film_url)
+    film_hash = JSON.parse(film_info)
+  end
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -18,14 +53,62 @@ def get_character_movies_from_api(character)
   #  of movies by title. play around with puts out other info about a given film.
 end
 
-def parse_character_movies(films_hash)
+def parse_character_movies(all_films_array)
   # some iteration magic and puts out the movies in a nice list
+  all_films_array.each do |film_hash|
+    puts film_hash["title"]
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
   parse_character_movies(films_hash)
 end
+
+def show_movie_crawl(movie)
+  #make the web request
+  all_movies = RestClient.get('http://www.swapi.co/api/films/')
+  movies_hash = JSON.parse(all_movies)
+
+  found_movie = movies_hash["results"].find {|movie_data| movie_data["title"] == movie }
+
+  system('clear')
+  star_wars_logo
+  sleep(2)
+  found_movie["opening_crawl"].split("\r\n").each do |line|
+    puts "          " + line
+    sleep(1)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+def star_wars_logo
+ puts "\r\n
+     ________________.  ___     .______
+    /                | /   \\    |   _  \\
+   |   (-----|  |----`/  ^  \\   |  |_)  |
+     \\   \\   |  |    /  /_\\  \\  |      /
+.-----)   |  |  |   /  _____  \\ |  |\\  \\-------.
+|________/   |__|  /__/     \\__\\| _| `.________|
+____    __    ____  ___     .______    ________.
+\\   \\  /  \\  /   / /   \\    |   _  \\  /        |
+ \\   \\/    \\/   / /  ^  \\   |  |_)  ||   (-----`
+  \\            / /  /_\\  \\  |      /  \\   \\
+   \\    /\\    / /  _____  \\ |  |\\  \\---)   |
+    \\__/  \\__/ /__/     \\__\\|__| `._______/"
+    puts ""
+    puts ""
+    puts ""
+end
+
 
 ## BONUS
 
